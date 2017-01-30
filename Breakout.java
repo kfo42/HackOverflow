0b321fd9251163f6d663e05e99534db12b6a97ca
S <s>
2017-01-30 14:52:27 -0800

    {"types":"C","runs":48}

--- Breakout.java
+++ Breakout.java
@@ -125,6 +125,7 @@ public class Breakout extends GraphicsProgram {
 		}
 		GLabel end = new GLabel ("GAME OVER");
 		end.setFont("Courier New-Bold-90");
+		end.setVisible(true);
 		add (end, (getHeight()-end.getAscent())/2, (getWidth()-
 				end.getWidth())/2);
 				
