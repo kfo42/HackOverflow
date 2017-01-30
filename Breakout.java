4df9c9233be45b9ada390a44b674cafa23297d53
S <s>
2017-01-30 13:40:16 -0800

    {"types":"C","runs":14}

--- Breakout.java
+++ Breakout.java
@@ -17,76 +17,77 @@ import java.awt.event.*;
 
 public class Breakout extends GraphicsProgram {
 
-/** Width and height of application window in pixels.  IMPORTANT NOTE:
-  * ON SOME PLATFORMS THESE CONSTANTS MAY **NOT** ACTUALLY BE THE DIMENSIONS
-  * OF THE GRAPHICS CANVAS.  Use getWidth() and getHeight() to get the 
-  * dimensions of the graphics canvas. */
+	/** Width and height of application window in pixels.  IMPORTANT NOTE:
+	 * ON SOME PLATFORMS THESE CONSTANTS MAY **NOT** ACTUALLY BE THE DIMENSIONS
+	 * OF THE GRAPHICS CANVAS.  Use getWidth() and getHeight() to get the 
+	 * dimensions of the graphics canvas. */
 	public static final int APPLICATION_WIDTH = 400;
 	public static final int APPLICATION_HEIGHT = 600;
 
-/** Dimensions of game board.  IMPORTANT NOTE: ON SOME PLATFORMS THESE 
-  * CONSTANTS MAY **NOT** ACTUALLY BE THE DIMENSIONS OF THE GRAPHICS
-  * CANVAS.  Use getWidth() and getHeight() to get the dimensions of
-  * the graphics canvas. */
+	/** Dimensions of game board.  IMPORTANT NOTE: ON SOME PLATFORMS THESE 
+	 * CONSTANTS MAY **NOT** ACTUALLY BE THE DIMENSIONS OF THE GRAPHICS
+	 * CANVAS.  Use getWidth() and getHeight() to get the dimensions of
+	 * the graphics canvas. */
 	private static final int WIDTH = APPLICATION_WIDTH;
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
-/** Dimensions of the paddle */
+	/** Dimensions of the paddle */
 	private static final int PADDLE_WIDTH = 60;
 	private static final int PADDLE_HEIGHT = 10;
 
-/** Offset of the paddle up from the bottom */
+	/** Offset of the paddle up from the bottom */
 	private static final int PADDLE_Y_OFFSET = 30;
 
-/** Number of bricks per row */
+	/** Number of bricks per row */
 	private static final int NBRICKS_PER_ROW = 10;
 
-/** Number of rows of bricks */
+	/** Number of rows of bricks */
 	private static final int NBRICK_ROWS = 10;
 
-/** Separation between bricks */
+	/** Separation between bricks */
 	private static final int BRICK_SEP = 4;
 
-/** Width of a brick */
+	/** Width of a brick */
 	private static final int BRICK_WIDTH =
-	  (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+			(WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
-/** Height of a brick */
+	/** Height of a brick */
 	private static final int BRICK_HEIGHT = 8;
 
-/** Radius of the ball in pixels */
+	/** Radius of the ball in pixels */
 	private static final int BALL_RADIUS = 10;
 
-/** Offset of the top brick row from the top */
+	/** Offset of the top brick row from the top */
 	private static final int BRICK_Y_OFFSET = 70;
 
-/** Number of turns */
+	/** Number of turns */
 	private static final int NTURNS = 3;
 
-/* Method: run() */
-/** Runs the Breakout program. */
+	/* Method: run() */
+	/** Runs the Breakout program. */
 	public void run() {
-			setUpBricks();
-			addMouseListeners();
-			
+		setUpBricks();
+		addMouseListeners();
+
 
 
 	}
-		public void mouseMoved(MouseEvent e) {
-			GRect paddle = new GRect (PADDLE_WIDTH, PADDLE_HEIGHT);
-			paddle.setFilled(true);
-			paddle.setFillColor(Color.BLACK);
-			paddle.setVisible(true);
-			int x = e.getX();
-			int y = e.getY();
-			add (paddle);
-			paddle.setLocation(x, y);
-			paddle.setVisible(false);}
+	public void mouseMoved(MouseEvent e) {
+		GRect paddle = new GRect (PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setFillColor(Color.BLACK);
+		paddle.setVisible(true);
+		int x = e.getX();
+		int y = e.getY();
+		add (paddle);
+		paddle.setLocation(x, y);
+		paddle.setVisible(false);
+	}
 
 
-			
-		}
-		private void setUpBricks(){
+
+
+	private void setUpBricks(){
 		double brickCols = NBRICKS_PER_ROW;
 		//Total number of bricks in the current row.
 		double brickRows = NBRICK_ROWS;
@@ -94,7 +95,7 @@ public class Breakout extends GraphicsProgram {
 		double width = BRICK_WIDTH;
 		double x_brick = 0;
 		double y_brick = 0;
-		
+
 		while (brickCols != 0){
 			while (brickRows != 0) {
 				x_brick = midpoint-(width*(brickCols/2));
@@ -111,7 +112,7 @@ public class Breakout extends GraphicsProgram {
 			y_brick = y_brick-BRICK_HEIGHT-BRICK_Y_OFFSET;
 		}
 	}
-		
-		/* You fill this in, along with any subsidiary methods */
-	}
+
+	/* You fill this in, along with any subsidiary methods */
+}
 
