3c6c675dca6e6b808b754f908828cac629f82376
S <s>
2017-01-30 14:30:09 -0800

    {"types":"C","runs":42}

--- Breakout.java
+++ Breakout.java
@@ -91,7 +91,7 @@ public class Breakout extends GraphicsProgram {
 
 			// update visualization
 			ball.move(vx, vy);
-			pause(5);
+			pause(8);
 			double x = ball.getX();
 			double y = ball.getY();
 			GObject collider = getCollidingObject(x,y);
