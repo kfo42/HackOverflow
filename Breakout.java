d307bd4048c7d9f512e6d6912897ceca278b2689
S <s>
2017-02-05 21:50:32 -0800

    {"types":"C","runs":183}

--- Breakout.java
+++ Breakout.java
@@ -344,7 +344,7 @@ public class Breakout extends GraphicsProgram {
 		end.setVisible(true);
 		end.setFont("Courier New-Bold-40");
 		add (end, getWidth()/2-end.getWidth()/2, getHeight()/2);
-		GLabel total = new GLabel ("Points: " + (100-bricksHit)+" / 620");
+		GLabel total = new GLabel ("Points: " + bricksHit+" / 620");
 		total.setFont("Courier New-Bold-20");
 		add (total, getWidth()/2-total.getWidth()/2, getHeight()/2+200);
 		pause(800);
