4fa84c6275a0012cae3a656cfa037eaeee6df86b
S <s>
2017-02-05 23:38:12 -0800

    {"types":"C","runs":264}

--- Breakout.java
+++ Breakout.java
@@ -87,7 +87,7 @@ public class Breakout extends GraphicsProgram {
 			Color.CYAN, Color.CYAN};
 
 	private double vx = 0;
-	private double vy = 1;
+	private double vy = 2;
 	private int lives = 3;
 	private double bricksHit = 0;
 	private int ballRadius = 10;
