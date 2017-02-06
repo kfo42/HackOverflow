064633fd782cd32535e0412ac1a61751b97156cb
S <s>
2017-02-05 22:14:15 -0800

    {"types":"C","runs":203}

--- Breakout.java
+++ Breakout.java
@@ -32,11 +32,11 @@ public class Breakout extends GraphicsProgram {
 	private static final int HEIGHT = APPLICATION_HEIGHT;
 
 	/** Dimensions of the paddle */
-	private static final int PADDLE_WIDTH = 20;
-	private static final int PADDLE_HEIGHT = 50;
+	private static final int PADDLE_WIDTH = 60;
+	private static final int PADDLE_HEIGHT = 10;
 
 	/** Offset of the paddle up from the bottom */
-	private static final int PADDLE_Y_OFFSET = 80;
+	private static final int PADDLE_Y_OFFSET = 20;
 
 	/** Number of bricks per row */
 	private static final int NBRICKS_PER_ROW = 10;
