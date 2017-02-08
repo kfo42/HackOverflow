32a5a2fbc7dc5091b5e32057646cc2fc71651ed7
S <s>
2017-02-08 00:03:11 -0800

    {"types":"C","runs":26}

--- BreakoutExtensions.java
+++ BreakoutExtensions.java
@@ -229,8 +229,8 @@ public class BreakoutExtensions extends GraphicsProgram {
 						}
 						bounceClip.play();
 					}
-				//Secret powerup with near-infinite lives.
-				
+					//Secret powerup with near-infinite lives.
+
 				}else if (collider == livesLeft || collider == points ){
 					//Nothing occurs if the ball hits the text.
 
@@ -455,14 +455,14 @@ public class BreakoutExtensions extends GraphicsProgram {
 			add(coin);
 		}
 		if (coinInsert.getX()==getWidth()/2 && coinInsert.getY()==0){
-		GLabel easterEgg= new GLabel ("You get 9,000 LIVES!!!");
-		remove(coin);
-		coinSlot1.setFillColor(Color.YELLOW);
-		coinSlot1.setFillColor(Color.YELLOW);
-		pause(5000);
-		lives = 9000;
-		remove(easterEgg);
-		
+			GLabel easterEgg= new GLabel ("You get 9,000 LIVES!!!");
+			remove(coin);
+			coinSlot1.setFillColor(Color.YELLOW);
+			coinSlot1.setFillColor(Color.YELLOW);
+			pause(5000);
+			lives = 9000;
+			remove(easterEgg);
+		}
 		if (coinInsert ==coinSlot1){
 			remove(coin);
 			coinSlot1.setFillColor(Color.PINK);
