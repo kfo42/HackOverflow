526af677f523b48cfb7289497ca1eb02c795e3f3
S <s>
2017-04-15 16:46:33 -0700

    {"types":"C","runs":36}

--- LogicGameTest.java
+++ LogicGameTest.java
@@ -32,7 +32,7 @@ public class LogicGameTest extends GraphicsProgram {
 		public void run() {	
 			Ball ball = new Ball(BALL_COLOR);
 			GOval oval = new GOval (20, 20);
-			add(ball.addBall(), 0,0);
+			add(ball.addBall(),0,0);
 			ball.addBall();
 			waitForClick();
 			
