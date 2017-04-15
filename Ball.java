eabd884a97841bc882a92f02dbe8978878408643
S <s>
2017-04-15 15:30:50 -0700

    {"types":"C","runs":7}

--- Ball.java
+++ Ball.java
@@ -18,4 +18,48 @@ public class Ball extends GraphicsProgram{
 	public void move(double vx, double vy){
 		ballImg.move(vx, vy);
 	}
+	/**
+	 * Method: Hit Bottom Wall
+	 * -----------------------
+	 * Returns whether or not the given ball should bounce off
+	 * of the bottom wall of the window.
+	 */
+	private boolean hitBottomWall(GOval ballImg, double vy) {
+		if(vy < 0) return false;
+		return ballImg.getY() > getHeight() - ballImg.getHeight();
+	}
+
+	/**
+	 * Method: Hit Top Wall
+	 * -----------------------
+	 * Returns whether or not the given ball should bounce off
+	 * of the top wall of the window.
+	 */
+	private boolean hitTopWall(GOval ballImg, double vy) {
+		if(vy > 0) return false;
+		return ballImg.getY() <= 0;
+	}
+
+	/**
+	 * Method: Hit Right Wall
+	 * -----------------------
+	 * Returns whether or not the given ball should bounce off
+	 * of the right wall of the window.
+	 */
+	private boolean hitRightWall(GOval ballImg, double vx) {
+		if(vx < 0) return false;
+		return ballImg.getX() >= getWidth() - ballImg.getWidth();
+	}
+
+	/**
+	 * Method: Hit Left Wall
+	 * -----------------------
+	 * Returns whether or not the given ball should bounce off
+	 * of the left wall of the window.
+	 */
+	private boolean hitLeftWall(GOval ballImg, double vx) {
+		if(vx > 0) return false;
+		return ballImg.getX() <= 0;
+	}
+
 }
