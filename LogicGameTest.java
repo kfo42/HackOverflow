730b1a29d0a112884fe03bdf48f326b4790510e2
S <s>
2017-04-15 18:17:27 -0700

    {"types":"C","runs":39}

--- LogicGameTest.java
+++ LogicGameTest.java
@@ -61,8 +61,8 @@ public class LogicGameTest extends GraphicsProgram {
 					bucketHits++;
 					if ((bucket.getHeight()*bucketHits)<getHeight()){
 						bucket.setSize(bucket.getWidth(), bucket.getHeight()*.6*bucketHits);
-						if (getHeight()-(bucket.getHeight()*bucketHits)/2>0){
-							bucket.setLocation(getWidth()/2-bucket.getWidth()/2, (getHeight()-(bucket.getHeight()*bucketHits))/2);
+						if (getHeight()-(bucket.getHeight()*bucketHits*.6)/2>0){
+							bucket.setLocation(getWidth()/2-bucket.getWidth()/2, (getHeight())-(bucket.getHeight()*bucketHits*.6)/2);
 						}
 					}
 				}
